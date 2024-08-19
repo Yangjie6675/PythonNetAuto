32c1d20815e8682e52fbf58a331395ea26e722cb
BlueStorm <154657796+Yangjie6675@users.noreply.github.com>
2024-08-19 13:16:03 +0800

    Add files via upload

---
+++ A001.py
@@ -0,0 +1,26 @@
+a = 'you are my best friend'
+
+print(a)
+
+First_Net = "This is my first python code !"
+
+print(First_Net)
+
+# 变量
+a = 1
+b = 'CISCO'
+c = [1, 2, 3, 4,5]
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+cisco = b.lower()
+print(cisco)
+
+aa = cisco.upper()
+print(aa)
+
+aaa = str(a) + b
+
+print(aaa)
